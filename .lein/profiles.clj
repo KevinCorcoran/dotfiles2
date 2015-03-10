7bfdd3e20a34e3f189aeb2ba2a7b5eb1de0f5a43
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2015-03-10 15:32:55 -0700

    upgrade aviso version, clean up .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,2 @@
-{ :user { 
-  :dependencies [[io.aviso/pretty "0.1.12" 
-                  :exclusions [org.clojure/clojure]]] ; depends on 1.6 but our stuff is still on 1.5.1
-  :repl-options {
-    :nrepl-middleware [io.aviso.nrepl/pretty-middleware]}}}
+{:user {:dependencies [[io.aviso/pretty "0.1.17" ]]
+ :repl-options {:nrepl-middleware [io.aviso.nrepl/pretty-middleware]}}}
