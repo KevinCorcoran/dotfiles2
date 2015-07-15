eae9a2c4610f2151f4a5db53b3378b2472b34559
Kevin Corcoran <kwcorcoran@gmail.com>
2015-07-15 13:03:39 -0700

    make git compare-with-remote better

--- bin/compare_with_remote.rb
+++ bin/compare_with_remote.rb
@@ -1,21 +1,28 @@
-def log_it(commits, name)
+def log_it(commits, name, verbose_name)
 	if commits.empty?
-		puts "No unique commits exist on the #{name} branch."
+		puts "No unique commits exist on the #{name} branch (#{verbose_name})."
 	else
-		puts "These commits only exist on the #{name} branch:"
+		puts "These commits only exist on the #{name} branch (#{verbose_name}):"
 		puts commits
 	end
 end
 
 current_branch = `git current-branch`.chomp
+potential_tracking_branch = `git tracking`.chomp
+tracking_branch = 
+	if $? == 0
+		potential_tracking_branch
+	else
+		"origin/#{current_branch}"
+	end
 
 puts
 
-remote_commits = `git log --color=always --oneline ..origin/#{current_branch}`
-log_it(remote_commits, "REMOTE")
+remote_commits = `git log --color=always --oneline ..#{tracking_branch}`
+log_it(remote_commits, "REMOTE", tracking_branch)
 
 puts
 
-local_commits = `git log --color=always --oneline origin/#{current_branch}..`
-log_it(local_commits, "LOCAL")
+local_commits = `git log --color=always --oneline #{tracking_branch}..`
+log_it(local_commits, "LOCAL", current_branch)
 
